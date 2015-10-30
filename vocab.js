0689d573c3f84ea67412984055067bba50072d55
damianc <damianczapiewski@gmail.com>
2015-10-30 14:53:19 +0100

    First draft.

--- vocab.js
+++ vocab.js
@@ -1,20 +1,20 @@
 vocab = {
-	'zm': 'var',
-	'stała': 'const',
+    'zm': 'var',
+    'stała': 'const',
     'funkcja': 'function',
     'zwróć': 'return',
     'generator': 'function*',
     'dostarcz': 'yield',
     'jeśli': 'if',
     'inaczej': 'else',
-	'przez': 'for',
-	'podczas': 'while',
-	'dopóki': 'while',
-	'tak': 'true',
-	'nie': 'false',
-	'wyjdź': 'break',
-	'jedź': 'continue',
-	'kontynuuj': 'continue'
+    'przez': 'for',
+    'podczas': 'while',
+    'dopóki': 'while',
+    'tak': 'true',
+    'nie': 'false',
+    'wyjdź': 'break',
+    'jedź': 'continue',
+    'kontynuuj': 'continue'
 };
 
 module.exports = vocab;
